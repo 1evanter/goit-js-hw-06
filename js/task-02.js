3580a078a846b35651d02cd2819e876b1f187007
1evanter <rita.oleksenkoo@gmail.com>
2023-06-05 19:06:38 +0200

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listOfIngredients = document.querySelector('#ingredients');
+
+
+const itemsRef = ingredients.map(ingredient => {
+  const itemOfListRef = document.createElement('li');
+  itemOfListRef.textContent = ingredient;
+  itemOfListRef.classList.add('item');
+
+  return itemOfListRef;
+});
+
+listOfIngredients.append(...itemsRef);
+
+console.log(listOfIngredients);
+
+
