9884058822207c2fc73b293178c55f8866121460
1evanter <rita.oleksenkoo@gmail.com>
2023-06-06 15:35:18 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changecolorBtnEl = document.querySelector('.change-color');
+const widgetEl = document.querySelector('.widget');
+const colorTextEl = document.querySelector('.color');
+
+changecolorBtnEl.addEventListener('click', () => {
+  widgetEl.style.backgroundColor = getRandomHexColor();
+  colorTextEl.textContent = `${widgetEl.style.backgroundColor}`;
+})
