813a4a0ca5a3e361710dd8481f540ca22a5eb301
1evanter <rita.oleksenkoo@gmail.com>
2023-06-06 16:17:25 +0200

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,30 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+inputEl: document.querySelector('#controls input'),
+createBtnEl: document.querySelector('[data-create]'),
+  destroyBtnEl: document.querySelector('[data-destroy]'),
+  boxesEl: document.querySelector('#boxes'),
+}
+
+refs.createBtnEl.addEventListener('click', handleBoxCreate);
+
+function handleBoxCreate() {
+  for (let i = 0; i < refs.inputEl.value; i += 1) {
+    const newBox = document.createElement('div');
+    refs.boxesEl.append(newBox);
+
+    newBox.style.backgroundColor = getRandomHexColor();
+    newBox.style.width = 60 +"px";
+    newBox.style.height = 60 +"px";
+  }
+}
+  
+refs.destroyBtnEl.addEventListener('click', handleBoxDestroy);
+
+function handleBoxDestroy() {
+  refs.boxesEl.innerHTML = '';
+  refs.inputEl.value = '';
+}
