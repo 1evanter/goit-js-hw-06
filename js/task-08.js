2ea4bfbc1b32d6050da52b81b88a6bb196733b22
1evanter <rita.oleksenkoo@gmail.com>
2023-06-06 15:22:35 +0200

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const loginFormEl = document.querySelector('.login-form');
 
+loginFormEl.addEventListener('submit', handleFormSubmit);
+
+const formInfo = {};
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+ 
+    const email = event.currentTarget.elements.email;
+    const password = event.currentTarget.elements.password;
+    
+    if (email.value === '' || password.value === '') {
+        alert('Fill in all the fields.')
+    } else {
+        formInfo.email = email.value;
+        formInfo.password = password.value;
+    }
+
+    console.log(formInfo);
+ event.currentTarget.reset();
+}
+ 
