d1c1aae745901923117590dfeb05226382ba5052
1evanter <rita.oleksenkoo@gmail.com>
2023-06-07 11:13:25 +0200

    update task-07

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,8 @@
 const rangeInputEl = document.querySelector('#font-size-control');
 const inputTextEl = document.querySelector('#text');
 
+inputTextEl.style.fontSize = rangeInputEl.value + 'px';
+
 rangeInputEl.addEventListener('input', event => {
     inputTextEl.style.fontSize = rangeInputEl.value + 'px';
 });
